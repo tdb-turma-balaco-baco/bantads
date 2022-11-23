9a6b3638b65e51048d9fa205994f266679bcf232
Gabril <gjperes@ufpr.br>
2022-11-22 22:15:39 -0300

    Links com navegação + exib. por perfil

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -2,20 +2,34 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Perfil } from '../shared';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
 })
 export class SidenavComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private router: Router) {}
 
+  // TODO: Implementar o método
+  currentProfilePrivilege(): Perfil {
+    return 'CLIENTE';
+    // return 'ADMIN';
+    // return 'GERENTE';
+  }
+
+  logout() {
+    // this.authService.logout();
+    console.log("Logout");
+    // this.router.navigate(['/login']);
+  }
 }
