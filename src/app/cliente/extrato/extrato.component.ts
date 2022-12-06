9f12e4346572fcaba6c451736704594e58d05865
Eduardo Santana <eduardomsantana@outlook.com.br>
2022-12-06 14:27:31 -0300

    init componente extrato

---
+++ src/app/cliente/extrato/extrato.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extrato',
+  templateUrl: './extrato.component.html',
+  styleUrls: ['./extrato.component.css']
+})
+export class ExtratoComponent {
+
+}
