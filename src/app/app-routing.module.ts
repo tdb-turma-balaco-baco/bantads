ad09a0a99ef9227e221283091dd334e369c56b35
Eduardo Santana <eduardomsantana@outlook.com.br>
2022-12-17 23:43:47 -0300

    fix: Removida sidebar da tela de login utiliznado layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,41 @@ import { AuthRoutes } from './auth/auth-routing.module';
 import { ClienteRoutes } from './cliente';
 import { gerenteRoutes } from './gerente/gerente-routing.module';
 import { adminRoutes } from './admin/admin-routing.module';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+import { LoginComponent } from './auth/login/login.component';
+import { CadastroComponent } from './auth/cadastro/cadastro.component';
+
+
 const routes: Routes = [
   // TODO: Atualizar o redirectTo do root
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  ...adminRoutes,
-  ...AuthRoutes,
-  ...ClienteRoutes,
-  ...gerenteRoutes,
+  {
+    path: 'login',
+    component: LoginLayoutComponent,
+    children:[
+      { path: '', pathMatch: 'full', component: LoginComponent }
+    ]
+  },
+  {
+    path: 'cadastro',
+    component: LoginLayoutComponent,
+    children:[
+      { path: '', pathMatch: 'full', component: CadastroComponent }
+    ]
+  },
+  {
+    path: '',
+    component: HomeLayoutComponent,
+    children:[
+
+
+      ...adminRoutes,
+      ...AuthRoutes,
+      ...ClienteRoutes,
+      ...gerenteRoutes,
+    ]
+  },
 ];
 
 
